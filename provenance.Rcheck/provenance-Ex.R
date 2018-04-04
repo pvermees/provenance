eb258404bdcc142e4344802ee43473701a5e9977
pvermees <pieter.vermeesch@gmail.com>
2018-04-04 23:26:54 +0100

    Added option for ternary confidence intervals for central composition

--- provenance.Rcheck/provenance-Ex.R
+++ provenance.Rcheck/provenance-Ex.R
@@ -1112,7 +1112,7 @@ base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 ### ** Examples
 
 data(Namib)
-tern <- ternary(Namib$PT,'Q',c('KF','P'),c('Lm','Lv','Ls'))
+tern <- ternary(Namib$Major,'CaO','Na2O','K2O')
 plot(tern)
 ternary.ellipse(tern)
 
@@ -1133,7 +1133,8 @@ base::assign(".ptime", proc.time(), pos = "CheckExEnv")
 
 ### ** Examples
 
-tern <- ternary(Namib$PT,'Q',c('KF','P'),c('Lm','Lv','Ls'))
+data(Namib)
+tern <- ternary(Namib$Major,'CaO','Na2O','K2O')
 plot(tern,pch=21,bg='red',labels=NULL)
 # add the geometric mean composition as a text label:
 gmean <- ternary(exp(colMeans(log(tern$x))))
