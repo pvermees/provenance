b8cc3804427c2629ff3a5eb134ad2292104a61fb
pvermees <p.vermeesch@ucl.ac.uk>
2019-03-17 17:30:06 +0000

    Added dispersion limits to output of central() function

--- R/radialplot.R
+++ R/radialplot.R
@@ -137,6 +137,8 @@ x2zs <- function(x){
 #'               distribution. \code{central} computes a continuous
 #'               mixture model for each component (column)
 #'               separately. Covariance terms are not reported.}
+#' \item{LL}{ the lower limit of a `1 sigma' region for  code{theta}. }
+#' \item{UL}{ the upper limit of a `1 sigma' region for  code{theta}. }
 #' \item{mswd}{ the mean square of the weighted deviates, a.k.a.
 #'              reduced chi-square statistic.}
 #' \item{p.value}{ the p-value for age homogeneity }
@@ -147,9 +149,9 @@ central <- function(x,...){
     else dat <- x$x
     ns <- nrow(dat)
     nc <- ncol(dat)
-    out <- matrix(0,5,nc)
+    out <- matrix(0,7,nc)
     colnames(out) <- colnames(dat)
-    rownames(out) <- c('theta','err','sigma','mswd','p.value')
+    rownames(out) <- c('theta','err','sigma','LL','UL','mswd','p.value')
     for (i in 1:nc){
         Nsj <- subset(dat,select=i)
         Nij <- rowSums(subset(dat,select=-i))
@@ -163,8 +165,8 @@ central.multivariate <- function(x,...){
     else dat <- x$x
     ns <- nrow(dat)
     nc <- ncol(dat)
-    out <- matrix(0,5,nc-1)
-    rownames(out) <- c('theta','err','sigma','mswd','p.value')
+    out <- matrix(0,7,nc-1)
+    rownames(out) <- c('theta','err','sigma','LL','UL','mswd','p.value')
     Nij <- subset(dat,select=nc) # use last column as denominator
     for (i in 1:(nc-1)){ # loop over all but the last column
         Nsj <- subset(dat,select=i)
@@ -178,8 +180,8 @@ central_helper <- function(Nsj,Nij){
     sigma <- 0.15 # convenient starting value
     Ns <- sum(Nsj)
     Ni <- sum(Nij)
-    if (Ns==0) return(c(0,0,0,1,1))
-    if (Ni==0) return(c(1,0,0,1,1))
+    if (Ns==0) return(c(0,0,0,0,0,1,1))
+    if (Ni==0) return(c(1,0,0,0,0,1,1))
     mj <- Nsj+Nij
     ispos <- (mj>0)
     pj <- 0*Nsj
@@ -206,7 +208,10 @@ central_helper <- function(Nsj,Nij){
     mswd <- Chi2/df    
     p.value <- 1-stats::pchisq(Chi2,df)
     err <- 1/(sqrt(sum(wj))*(1-theta)^2)
-    c(theta,err,sigma,mswd,p.value)
+    mu <- log(theta)-log(1-theta)
+    LL <- exp(mu-sigma)/(1+exp(mu-sigma))
+    UL <- exp(mu+sigma)/(1+exp(mu+sigma))
+    c(theta,err,sigma,LL,UL,mswd,p.value)
 }
 
 # maximum likelihood alternative to central_helper(Nsj,Nij)
