0c7b953ea6f82458016c4c7fcfad22fd652062a0
pvermees <p.vermeesch@ucl.ac.uk>
2019-12-05 14:24:02 +0000

    Added graded colour option to correspondence analysis

--- R/plot.R
+++ R/plot.R
@@ -184,36 +184,85 @@ plot.GPA <- function(x,pch=NA,pos=NULL,col='black',bg='white',cex=1,...){
 #'
 #' Plot the results of a principal components analysis as a biplot
 #' @param x an object of class \code{PCA}
-#' @param ... optional arguments of the \code{biplot} function
+#' @param levels a vector of length \code{nrow(dat$x)} with values
+#'     that are to be used for the colour scale
+#' @param labelcol two element vector with the colours that are to be
+#'     assigned to the labels of the samples corresponding to the
+#'     minimum and maximum value of \code{levels}. If
+#'     \code{levels==NULL}, then the first item of the vector is used
+#'     for all sample labels.
+#' @param vectorcol colour of the vector loadings for the variables
+#' @param choices see the help pages of the
+#'     \code{\link[stats]{biplot}} function.
+#' @param scale see the help pages of the \code{\link[stats]{biplot}}
+#'     function.
+#' @param pc.biplot see the help pages of the
+#'     \code{\link[stats]{biplot}} function.
+#' @param ... optional arguments of the generic
+#'     \code{\link[stats]{biplot}} function
 #' @examples
 #' data(Namib)
 #' plot(PCA(Namib$Major))
 #' @seealso PCA
 #' @method plot PCA
 #' @export
-plot.PCA <- function(x,levels=NULL,choices=1L:2L,
-                     scale=1,pc.biplot=FALSE,...){
-    PCAbiplotHelper(x, levels=levels, choices=choices,
-                    scale=scale, pc.biplot=pc.biplot, ...)
+plot.PCA <- function(x, levels=NULL, labelcol=c('black','blue'),
+                     vectorcol='red', choices = 1L:2L,
+                     scale = 1, pc.biplot = FALSE, ...){
+    if (length(choices) != 2L) 
+        stop("length of choices must be 2")
+    if (!length(scores <- x$x)) 
+        stop(gettextf("object '%s' has no scores", deparse(substitute(x))), 
+             domain = NA)
+    if (is.complex(scores)) 
+        stop("biplots are not defined for complex PCA")
+    lam <- x$sdev[choices]
+    n <- NROW(scores)
+    lam <- lam * sqrt(n)
+    if (scale < 0 || scale > 1) 
+        warning("'scale' is outside [0, 1]")
+    if (scale != 0) 
+        lam <- lam^scale
+    else lam <- 1
+    if (pc.biplot) 
+        lam <- lam/sqrt(n)
+    biplotHelper(t(t(scores[, choices])/lam),
+                 t(t(x$rotation[, choices]) * lam),
+                 levels=levels, labelcol=labelcol,
+                 vectorcol=vectorcol,...)
+    IsoplotR:::colourbar(z=levels,col=labelcol)
+    invisible()
 }
 
 #' Point-counting biplot
 #'
 #' Plot the results of a correspondence analysis as a biplot
 #' @param x an object of class \code{CA}
-#' @param ... optional arguments of the \code{biplot} function
+#' @param levels a vector of length \code{nrow(dat$x)} with values
+#'     that are to be used for the colour scale
+#' @param labelcol two element vector with the colours that are to be
+#'     assigned to the labels of the samples corresponding to the
+#'     minimum and maximum value of \code{levels}. If
+#'     \code{levels==NULL}, then the first item of the vector is used
+#'     for all sample labels.
+#' @param vectorcol colour of the vector loadings for the variables
+#' @param ... optional arguments of the generic
+#'     \code{\link[stats]{biplot}} function
 #' @examples
 #' data(Namib)
 #' plot(CA(Namib$PT))
 #' @seealso CA
 #' @method plot CA
 #' @export
-plot.CA <- function(x,...){
+plot.CA <- function(x,levels=NULL,labelcol=c('black','blue'),vectorcol='red',...){
     X <- x$rscore[, 1L:2]
     X <- X %*% diag(x$cor[1L:2])
     Y <- x$cscore[, 1L:2]
     Y <- Y %*% diag(x$cor[1L:2])
-    stats::biplot(X,Y,xlab='Component 1',ylab='Component 2',...)
+    biplotHelper(X, Y, levels=levels, labelcol=labelcol, vectorcol=vectorcol,
+                 xlab='Component 1',ylab='Component 2',...)
+    IsoplotR:::colourbar(z=levels,col=labelcol)
+    invisible()
 }
 
 #' Plot an MDS configuration
@@ -652,36 +701,10 @@ saveplot <- function(f, d){
     grDevices::dev.copy2pdf(file=f)
 }
 
-PCAbiplotHelper <- function(x, levels=NULL, labelcol=c('black','blue'),
-                            vectorcol='red', choices = 1L:2L,
-                            scale = 1, pc.biplot = FALSE, ...){
-    if (length(choices) != 2L) 
-        stop("length of choices must be 2")
-    if (!length(scores <- x$x)) 
-        stop(gettextf("object '%s' has no scores", deparse(substitute(x))), 
-             domain = NA)
-    if (is.complex(scores)) 
-        stop("biplots are not defined for complex PCA")
-    lam <- x$sdev[choices]
-    n <- NROW(scores)
-    lam <- lam * sqrt(n)
-    if (scale < 0 || scale > 1) 
-        warning("'scale' is outside [0, 1]")
-    if (scale != 0) 
-        lam <- lam^scale
-    else lam <- 1
-    if (pc.biplot) 
-        lam <- lam/sqrt(n)
-    biplotHelper(t(t(scores[, choices])/lam),
-                 t(t(x$rotation[, choices]) * lam),
-                 levels=levels, labelcol=labelcol,
-                 vectorcol=vectorcol,...)
-    IsoplotR:::colourbar(z=levels,col=labelcol)
-    invisible()
-}
-
-biplotHelper <- function(x, y, var.axes = TRUE, levels=NULL, labelcol='black',
-                         vectorcol='red', cex = rep(par("cex"), 2), xlabs = NULL,
+# modified from stats::biplot.default
+biplotHelper <- function(x, y, var.axes = TRUE,
+                         levels=NULL, labelcol=c('black','blue'),
+                         vectorcol='red', cex = rep(graphics::par("cex"), 2), xlabs = NULL,
                          ylabs = NULL, expand = 1, xlim = NULL, ylim = NULL,
                          arrow.len = 0.1, main = NULL, sub = NULL,
                          xlab = NULL, ylab = NULL, ...){
@@ -721,24 +744,24 @@ biplotHelper <- function(x, y, var.axes = TRUE, levels=NULL, labelcol='black',
     else if (missing(ylim)) 
         ylim <- rangx2
     ratio <- max(rangy1/rangx1, rangy2/rangx2)/expand
-    on.exit(par(op))
-    op <- par(pty = "s")
+    on.exit(graphics::par(op))
+    op <- graphics::par(pty = "s")
     if (!is.null(main)) 
-        op <- c(op, par(mar = par("mar") + c(0, 0, 1, 0)))
-    plot(x, type = "n", xlim = xlim, ylim = ylim, col = lcol, 
-        xlab = xlab, ylab = ylab, sub = sub, main = main, ...)
-    text(x, xlabs, cex = cex[1L], col = lcol, ...)
-    par(new = TRUE)
-    dev.hold()
-    on.exit(dev.flush(), add = TRUE)
-    plot(y, axes = FALSE, type = "n", xlim = xlim * ratio, ylim = ylim * 
-        ratio, xlab = "", ylab = "", col = vectorcol, ...)
-    axis(3, col = vectorcol, ...)
-    if (is.null(levels)) axis(4, col = vectorcol, ...)
-    box(col = 'black')
-    text(y, labels = ylabs, cex = cex[2L], col = vectorcol, ...)
+        op <- c(op, graphics::par(mar = graphics::par("mar") + c(0, 0, 1, 0)))
+    graphics::plot(y, axes = FALSE, type = "n", xlim = xlim * ratio,
+                   ylim = ylim * ratio, xlab = "", ylab = "", col = vectorcol, ...)
+    graphics::axis(3, col = vectorcol, ...)
+    if (is.null(levels)) graphics::axis(4, col = vectorcol, ...)
+    graphics::box(col = 'black')
+    graphics::text(y, labels = ylabs, cex = cex[2L], col = vectorcol, ...)
     if (var.axes) 
-        arrows(0, 0, y[, 1L] * 0.8, y[, 2L] * 0.8, col = vectorcol, 
-            length = arrow.len)
+        graphics::arrows(0, 0, y[, 1L] * 0.8, y[, 2L] * 0.8,
+                         col = vectorcol, length = arrow.len)
+    graphics::par(new = TRUE)
+    grDevices::dev.hold()
+    on.exit(grDevices::dev.flush(), add = TRUE)
+    graphics::plot(x, type = "n", xlim = xlim, ylim = ylim, col = lcol, 
+                   xlab = xlab, ylab = ylab, sub = sub, main = main, ...)
+    graphics::text(x, xlabs, cex = cex[1L], col = lcol, ...)
     invisible()
 }
